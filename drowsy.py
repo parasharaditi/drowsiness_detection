89a5f7fff05bb2777dde57dbf8b4b8d108332df7
Aditi Parashar <47334754+parasharaditi@users.noreply.github.com>
2020-04-18 00:42:14 +0530

    Add files via upload

---
+++ drowsy.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 30 11:29:40 2020
+
+@author: KIIT
+"""
+
+from scipy.spatial import distance
+from imutils import face_utils
+import imutils
+import cv2
+import dlib
+import cv2
